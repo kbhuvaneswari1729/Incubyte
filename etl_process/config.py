2c46aa6f3ab52ce49fbb51de1906b0000033e964
kbhuvaneswari1729 <kbhuvaneswari1729@gmail.com>
2025-01-18 15:23:53 +0530

    Delete etl_process directory

--- etl_process/config.py
+++
@@ -1,8 +0,0 @@
-# Snowflake connection settings
-SNOWFLAKE_ACCOUNT = 'your_account'
-SNOWFLAKE_USER = 'your_user'
-SNOWFLAKE_PASSWORD = 'your_password'
-SNOWFLAKE_DATABASE = 'your_database'
-SNOWFLAKE_SCHEMA = 'your_schema'
-SNOWFLAKE_WAREHOUSE = 'your_warehouse'
-
